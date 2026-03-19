4ccc96767da6b59229a265b0ddf5aca30365b5af
zaky <muhammadzaky00@gmail.com>
2026-03-19 22:52:00 +0700

    fix errror handle saat login

--- src/main/java/com/zamagi/kas/controller/AuthController.java
+++ src/main/java/com/zamagi/kas/controller/AuthController.java
@@ -8,6 +8,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -35,15 +38,17 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public Map<String, String> authenticateUser(@RequestBody User user) {
-        User u = userRepository.findByUsername(user.getUsername())
-                .orElseThrow(() -> new RuntimeException("User tidak ditemukan"));
+    public ResponseEntity<?> authenticateUser(@RequestBody User user) {
+        var userOptional = userRepository.findByUsername(user.getUsername());
 
-        if (encoder.matches(user.getPassword(), u.getPassword())) {
-            String token = jwtUtils.generateJwtToken(u.getUsername());
-            return Map.of("token", token, "username", u.getUsername());
-        } else {
-            throw new RuntimeException("Password salah!");
+        if (userOptional.isPresent()) {
+            User u = userOptional.get();
+            if (encoder.matches(user.getPassword(), u.getPassword())) {
+                String token = jwtUtils.generateJwtToken(u.getUsername());
+                return ResponseEntity.ok(Map.of("token", token, "username", u.getUsername()));
+            }
         }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Username atau Password salah!"));
     }
 }
